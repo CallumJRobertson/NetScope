2dd683267569e5e693ef19b5182bedd9157a8ab7
CallumJRobertson <90865822+CallumJRobertson@users.noreply.github.com>
2026-03-11 13:55:27 +0000

    Initial Commit

---
+++ NetScope/NetScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetScopeApp.swift
+//  NetScope
+//
+//  Created by Callum Robertson on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NetScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
